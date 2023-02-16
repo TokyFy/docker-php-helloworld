0ad4ee2b5096a3e41384ac7fd35ff3bc14c61f6c
Toky <tokyfydev@gmail.com>
2023-02-16 11:28:18 +0100

    First commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM php:7.0-apache
-COPY src/ /var/www/html
+
+VOLUME /source
+
+COPY config/apache2.conf /etc/apache2/
+COPY config/000-default.conf /etc/apache2/sites-available/
+
 EXPOSE 80
 
+# docker build -t hellophp .
+# docker run -p 80:80 -v ${pwd}:/source hellophp
--- src/index.php
+++ src/index.php
@@ -1,7 +1,55 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello php</title>
 
-echo "Hello, World from Docker! <br>";
-echo "Hello ECS! <br>";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+            -ms-overflow-style: none;  /* IE and Edge */
+            scrollbar-width: none;  /* Firefox */
+        }
 
-?>
+        *::-webkit-scrollbar {
+            display: none;
+        }
+
+        body {
+            background-color: black;
+            color: white;
+            padding: 1rem;
+            width: 100vw;
+            height: 100vh;
+            font-family: monospace;
+            display: flex;
+            justify-content: center;
+            font-size: 2em;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $ARG = (int)$_GET["n"];
+
+        if(!$ARG) {
+            echo "THERE IS NO ARGS";
+        } else {
+           if($ARG < 100) {
+               for($i = 0 ; $i < $ARG ; $i++) {
+                   echo "$i : I love you! <br/>";
+               }
+           } else {
+               echo "Too long Broo";
+           }
+        }
+
+    ?>
+</body>
+</html>
+
